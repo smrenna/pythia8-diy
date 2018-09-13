c8760d926c50d35e4e3664913a406f1e7394e768
Holger Schulz <holger.schulz@durham.ac.uk>
2018-09-13 14:02:08 -0500

    Fix weight setting

--- withlheh5/withlheh5.cpp
+++ withlheh5/withlheh5.cpp
@@ -59,7 +59,7 @@ class LHAupH5 : public Pythia8::LHAup {
       _init        = file->getGroup("init");
       _procInfo    = file->getGroup("procInfo");
       // This reads and holds the information of readSize events, starting from firstEvent
-      //setInit();
+      setInit();
       lheevents = lheh5::readEvents(_index, _particle, _event, firstEvent, readSize);
      // Sum of trials for this block
      DataSet _trials     =  file->getDataSet("event/trials");
