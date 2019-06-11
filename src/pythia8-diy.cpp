1b924bf0de512c07c4413fd8184df74889886019
Holger Schulz <holger.schulz@durham.ac.uk>
2019-06-11 14:04:30 -0500

    small patch

--- src/pythia8-diy.cpp
+++ src/pythia8-diy.cpp
@@ -39,11 +39,14 @@
 
 #include "HepMC/IO_GenEvent.h"
 
-
+#include <highfive/H5DataSet.hpp>
+#include <highfive/H5DataSpace.hpp>
+#include <highfive/H5File.hpp>
 
 
 using namespace std;
 using namespace Pythia8;
+using namespace HighFive;
 #include "opts.h"
 
 using namespace std;
@@ -55,6 +58,42 @@ typedef diy::RegularGridLink RCLink;
 typedef GenericBlock<Bounds, PointConfig, AnalysisObjects> Block;
 typedef ConfigBlockAdder<Bounds, RCLink, Block> AddBlock;
 
+void treeVertex(HepMC::GenEvent& event) {
+    // Iterate over all vertices to find PS vertices
+    int vtx_id = -1;
+
+    for (HepMC::GenEvent::vertex_const_iterator vit=event.vertices_begin();
+       vit!=event.vertices_end(); ++vit) {
+      // Is this a PS Vertex?
+      if ((*vit)->id()==4) {
+        std::vector<HepMC::GenParticle*> remove;
+        //// Loop over outgoing particles
+        for (HepMC::GenVertex::particles_out_const_iterator pout
+               =(*vit)->particles_out_const_begin();
+             pout!=(*vit)->particles_out_const_end(); ++pout) {
+            if ( (*pout)->end_vertex() ) {
+              vtx_id = (*pout)->end_vertex()->id(); //
+              // Disconnect outgoing particle from end/decay vertex of type (1,2,3)
+              if (vtx_id==1 || vtx_id==2 || vtx_id==3 )
+                  remove.push_back((*pout));
+            }
+        }
+        // Loop over incoming particles
+        for (HepMC::GenVertex::particles_in_const_iterator pin
+               =(*vit)->particles_in_const_begin();
+             pin!=(*vit)->particles_in_const_end(); ++pin) {
+          vtx_id = (*pin)->production_vertex()->id();
+          // Disconnect incoming particle from production vertex of type (1,2,3)
+          if (vtx_id==1 || vtx_id==2 || vtx_id==3 )
+                  remove.push_back((*pin));
+        }
+        // Iterate over Genparticle pointers to remove from current vertex (*vit)
+        for (unsigned int nrem=0;nrem<remove.size();++nrem) {
+            (*vit)->remove_particle(remove[nrem]);
+        }
+      } // Close if statement (vertex id==4)
+    } // Close loop over vertices
+}
 
 void print_block(Block* b, const diy::Master::ProxyWithLink& cp)
 {
@@ -186,6 +225,63 @@ void write_yoda(Block* b, diy::Master::ProxyWithLink const& cp, int rank, bool v
   }
 }
 
+//void process_block_hepmc(Block* b, diy::Master::ProxyWithLink const& cp, bool verbose)
+//{
+  //// Explicit desctruction and recreation of pythia --- this is important when running multiple
+  //// physics configs!!! https://stackoverflow.com/questions/1124634/call-destructor-and-then-constructor-resetting-an-object
+  //(&b->pythia)->~Pythia();
+  //new (&b->pythia) Pythia();
+
+  //// Minimise pythia's output
+  //b->pythia.readString("Print:quiet = on");
+
+  //// Configure pythia with a vector of strings
+  //for (auto s  : b->state.conf) b->pythia.readString(s);
+
+  //// Py8 random seed for this block read from point config
+  //b->pythia.readString("Random:setSeed = on");
+  //b->pythia.readString("Random:seed = " + std::to_string(b->state.seed+cp.gid()));
+
+  //// All configurations done, initialise Pythia
+  //b->pythia.init();
+  //// Delete the AnalysisHandlerPtr to ensure there is no memory
+  //if (b->ah) { delete b->ah; }
+
+
+  //// The event loop
+  //b->events.reserve(b->state.num_events);
+  //int nAbort = 5;
+  //int iAbort = 0;
+  //if (verbose) fmt::print(stderr, "[{}] generating {} events\n", cp.gid(),  b->state.num_events);
+  //for (unsigned int iEvent = 0; iEvent < b->state.num_events; ++iEvent) {
+    //if (!b->pythia.next()) {
+      //if (++iAbort < nAbort) continue;
+      //break;
+    //}
+    //HepMC::GenEvent* hepmcevt = new HepMC::GenEvent();
+    //b->ToHepMC.fill_next_event( b->pythia, hepmcevt );
+    //b->events.push_back(hepmcevt);
+    //if (iEvent%1000 == 0 && cp.gid()==0) fmt::print(stderr, "[{}]  {}/{} \n", cp.gid(),  iEvent, b->state.num_events);;
+  //}
+
+  //for (auto e : b->events) {
+     ////treeVertex(&e);
+     //e->print();
+  //}
+  //// Push histos into block
+  ////b->data = b->ah->getData();
+
+  //// Debug write out --- uncomment to write each block's YODA file
+  ////b->ah->writeData(std::to_string((1+npc)*(b->state.seed+cp.gid()))+".yoda");
+
+
+  //// This is a bit annoying --- we need to unscale Histo1D and Histo2D beforge the reduction
+  //// TODO: Figure out whether this is really necessary
+//}
+
+
+
+
 void clear_buffer(Block* b, diy::Master::ProxyWithLink const& cp, bool verbose)
 {
   if (verbose) fmt::print(stderr, "[{}] -- clear buffer  \n", cp.gid());
@@ -199,12 +295,15 @@ void set_pc(Block* b,                             // local block
   if (cp.gid() == 0) b->state = pc;
 }
 
+
 // --- main program ---//
 int main(int argc, char* argv[])
 {
     diy::mpi::environment env(argc, argv);
     diy::mpi::communicator world;
+    
 
+    File *hdfoutput_file;
     size_t nBlocks = 0;
     int threads = 1;
     int runnum = -1;
@@ -220,6 +319,7 @@ int main(int argc, char* argv[])
     bool                      verbose     = ops >> Present('v',
                                                            "verbose",
                                                            "verbose output");
+    bool writehepmc   = ops >> Present('w', "writehepmc", "write hepmc events");
     ops >> Option('t', "thread",    threads,   "Number of threads");
     ops >> Option('n', "nevents",   nEvents,   "Number of events to generate in total");
     ops >> Option('b', "nblocks",   nBlocks,   "Number of blocks");
@@ -237,6 +337,34 @@ int main(int argc, char* argv[])
     }
 
     int nConfigs;
+
+    int batchsize=10;
+
+
+    //int data[1][2] = {{world.rank(), world.rank()}};
+    std::vector<int> data(50, world.rank());
+
+    if (writehepmc) {
+             hdfoutput_file = new File(out_file,
+			File::ReadWrite|File::Create|File::Truncate,
+			MPIOFileDriver(MPI_COMM_WORLD,MPI_INFO_NULL));
+             Group g_event = hdfoutput_file->createGroup("event");
+
+             size_t size(world.size());
+             std::vector<size_t> min(1,size);
+             min.front()*=nEvents;
+             std::vector<size_t> max(min);
+             DataSetCreateProps props;
+             max.front()=DataSpace::UNLIMITED;
+	     props.add(Chunking(std::vector<hsize_t>(1,batchsize*size)));
+             DataSet ds_nparticles = g_event.createDataSet<int>("nparticles",DataSpace(min,max),props);
+             ds_nparticles.select({std::size_t(world.rank()), 0}).write(data);
+             std::cout<<"kkk\n";
+             //ds_nparticles.write(data)();
+             //hdfoutput_file->close();
+             //
+
+    }
     
     std::vector<std::string> physConfig;
     std::vector<std::vector<std::string> > physConfigs;
@@ -331,22 +459,30 @@ int main(int argc, char* argv[])
        if (verbose) master.foreach([world](Block* b, const diy::Master::ProxyWithLink& cp)
                         {print_block(b, cp); });
 
-       master.foreach([world, verbose, ipc](Block* b, const diy::Master::ProxyWithLink& cp)
-                        {process_block(b, cp, verbose); });
+       //if (writehepmc) {
+          //master.foreach([world, verbose, ipc](Block* b, const diy::Master::ProxyWithLink& cp)
+                           //{process_block_hepmc(b, cp, verbose); });
+       //}
+
+       //else {
+
+          master.foreach([world, verbose, ipc](Block* b, const diy::Master::ProxyWithLink& cp)
+                           {process_block(b, cp, verbose); });
 
 
-       diy::reduce(master,              // Master object
-                   assigner,            // Assigner object
-                   partners,            // RegularMergePartners object
-                   &reduceData<Block>);
+          diy::reduce(master,              // Master object
+                      assigner,            // Assigner object
+                      partners,            // RegularMergePartners object
+                      &reduceData<Block>);
 
-       //////This is where the write out happens --- the output file name is part of the blocks config
-       master.foreach([world, verbose](Block* b, const diy::Master::ProxyWithLink& cp)
-                      { write_yoda(b, cp, world.rank(), verbose); });
+          //////This is where the write out happens --- the output file name is part of the blocks config
+          master.foreach([world, verbose](Block* b, const diy::Master::ProxyWithLink& cp)
+                         { write_yoda(b, cp, world.rank(), verbose); });
 
-       // Wipe the buffer to prevent double counting etc.
-       master.foreach([world, verbose](Block* b, const diy::Master::ProxyWithLink& cp)
-                      { clear_buffer(b, cp, verbose); });
+          // Wipe the buffer to prevent double counting etc.
+          master.foreach([world, verbose](Block* b, const diy::Master::ProxyWithLink& cp)
+                         { clear_buffer(b, cp, verbose); });
+       //}
    }
 
 
